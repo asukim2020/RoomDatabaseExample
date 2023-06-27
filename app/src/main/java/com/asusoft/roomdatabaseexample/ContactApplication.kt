8230f3c94fd733a01403571ec3bb05b8ca15cc2c
김현구 <hyeongu.kim@ipageon.com>
2023-06-27 15:10:26 +0900

    add DataStore Code 2

--- app/src/main/java/com/asusoft/roomdatabaseexample/ContactApplication.kt
+++ app/src/main/java/com/asusoft/roomdatabaseexample/ContactApplication.kt
@@ -1,15 +1,29 @@
 package com.asusoft.roomdatabaseexample
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.asusoft.roomdatabaseexample.room.ContactDatabase
 import com.asusoft.roomdatabaseexample.viewmodel.ContactRepository
+import com.ipageon.atnt.database.datastore.DataStoreManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
+
 class ContactApplication: Application() {
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_datastore")
 
     val applicationScope = CoroutineScope(SupervisorJob())
 
     val database by lazy { ContactDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { ContactRepository(database.contactDao()) }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DataStoreManager.dataStore = dataStore
+    }
 }
